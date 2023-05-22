b938910e92168f9cda0b7711aa2039933253342b
Joshuan <joshuan.chel@gmail.com>
2023-05-23 01:08:10 +0200

    Add render html with assets

--- services/www/src/server/routes.ts
+++ services/www/src/server/routes.ts
@@ -1,8 +1,31 @@
+import {createLayoutPlugin, createRenderFunction} from '@gravity-ui/app-layout';
 import calculator from 'calculator';
 import type {Request as ExpressRequest, Response as ExpressResponse} from 'express';
 
+const renderLayout = createRenderFunction([
+    createLayoutPlugin({
+        manifest: './dist/public/build/assets-manifest.json',
+        publicPath: '/build/',
+    }),
+]);
+
 export default {
     'GET /': (_req: ExpressRequest, res: ExpressResponse) => {
-        res.send(`This is WWW server! 1 + 2 = ${calculator(1, 2)}`);
+        res.send(
+            renderLayout({
+                title: 'WWW server',
+                bodyContent: {
+                    root: `1 + 2 = ${calculator(1, 2)}`,
+                },
+                data: {
+                    calc: [1, 2],
+                },
+                pluginsOptions: {
+                    layout: {
+                        name: 'index',
+                    },
+                },
+            }),
+        );
     },
 };
