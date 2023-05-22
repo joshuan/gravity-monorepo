4b3f31cf893baac0235ef3c0072ab28c44ac3d88
Joshuan <joshuan.chel@gmail.com>
2023-05-23 01:20:27 +0200

    Add translations on the client

--- services/www/src/ui/entries/index.ts
+++ services/www/src/ui/entries/index.ts
@@ -1,1 +1,31 @@
-console.log('I am client');
+import {I18N} from '@gravity-ui/i18n';
+
+import en from './keysets/en.json';
+import ru from './keysets/ru.json';
+
+const i18n = new I18N({
+    logger: console,
+});
+
+i18n.registerKeysets('ru', ru);
+i18n.registerKeysets('en', en);
+
+i18n.setLang('ru');
+
+const keyset = i18n.keyset('home');
+
+keyset('test');
+
+const root = document.getElementById('root');
+
+setTimeout(() => {
+    if (root) {
+        root.innerHTML = 'Minute passed!';
+    }
+}, 1000);
+
+setTimeout(() => {
+    if (root) {
+        root.innerHTML = keyset('root');
+    }
+}, 2000);
