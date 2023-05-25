c35811192bddf53d3712acf434d40a2c260dd5a5
Joshuan <joshuan.chel@gmail.com>
2023-05-25 12:01:32 +0200

    Stable build frontend

--- services/www/src/server/index.ts
+++ services/www/src/server/index.ts
@@ -1,5 +1,6 @@
 import {ExpressKit} from '@gravity-ui/expresskit';
 import {NodeKit} from '@gravity-ui/nodekit';
+import express from 'express';
 
 import routes from './routes';
 
@@ -8,5 +9,9 @@ const nodekit = new NodeKit({
 });
 
 const app = new ExpressKit(nodekit, routes);
+
+if (nodekit.config.appEnv === 'production') {
+    app.express.use('/build', express.static('dist/public/build'));
+}
 
 app.run();
